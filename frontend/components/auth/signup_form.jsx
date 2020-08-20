7154ae42082b3df7b925f928933bd451687c1fb9
Will Offit <will.offit@gmail.com>
2020-08-20 09:18:24 -0700

    no logger version

--- frontend/components/auth/signup_form.jsx
+++ frontend/components/auth/signup_form.jsx
@@ -17,7 +17,7 @@ class SignupForm extends React.Component {
   componentDidMount() {
     const input = document.getElementById('username');
     input.focus();
-    this.props.clearLoginErrors
+    this.props.clearLoginErrors();
   }
 
   handleSubmit(e) {
