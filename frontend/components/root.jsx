d15d26dcc5047b132e1f917078bb1656e2f91021
Will Offit <will.offit@gmail.com>
2020-08-28 10:39:32 -0700

    please

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,15 +2,11 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
 import React from 'react';
-import { ActionCableProvider } from 'react-actioncable-provider';
-import { API_WS_ROOT } from '../actions/channel_actions';
 
 export default ({store}) => (
-  <ActionCableProvider url={API_WS_ROOT}>
     <Provider store={store}>
       <HashRouter>
         <App />
       </HashRouter>
     </Provider>
-  </ActionCableProvider>
 )
